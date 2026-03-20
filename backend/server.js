6b8803c994ebb72c2cb59258ce5c6c2dfd5d9f97
24750Montenegro <mon24750@uvg.edu.gt>
2026-03-19 19:55:52 -0600

    fix: fallback eliminado para no usar variables harcodeadas.

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.json());
 
-const PORT = process.env.APP_PORT || 3000;
+const PORT = process.env.APP_PORT;  // si se puedieran agregar variables harcoded agregaría un fallback por si el puerto está usado
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
